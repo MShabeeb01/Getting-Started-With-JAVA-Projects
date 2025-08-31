3b928ebdc39d66c4d27d74d865deef27fec65b77
Mohammed Shabeeb <mohammed.shabeebbb@gmail.com>
2025-08-31 21:25:59 +0530

    Uses User Input and While Loop.

--- 04_Reverse_Number.java
+++ 04_Reverse_Number.java
@@ -7,7 +7,7 @@ public class Basic {
         int num = sc.nextInt();
         int rev = 0;
 
-        while (num > 0) {
+        while (num > 0) { 
             int lastDigit = num % 10; //This gives the last digit.
             rev = (rev * 10) + lastDigit; //This equation gives the reversed number.
             num = num / 10; //Will run until the number becomes zero
